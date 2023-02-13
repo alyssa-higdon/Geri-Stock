8d400aa22a1a235a0efe97f23fe32b3637286818
alexhatch <alexhatchh@gmail.com>
2023-02-13 13:07:29 -0800

    Changed connection link

--- Backend/models/user-services.js
+++ Backend/models/user-services.js
@@ -4,9 +4,10 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 mongoose.set("debug", true);
+mongoose.set('strictQuery', true);
 
 mongoose
-  .connect("mongodb+srv://"+process.env.MONGO_USER+":"+process.env.MONGO_PWD+"@cluster0.kpnxlin.mongodb.net/test", {
+  .connect("mongodb+srv://"+process.env.MONGO_USER+":"+process.env.MONGO_PWD+"@cluster0.kpnxlin.mongodb.net/Geri-Stock", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
