31328ad361b555d1dce2d85cadc7de9b9b7dd4fb
Kara Sawamura <sawamurakara@gmail.com>
2023-02-15 13:53:05 -0800

    Added .eslintrc.js configuration file

---
+++ .eslintrc.js
@@ -0,0 +1,6 @@
+  "format": "prettier --write './**/*.{js,jsx,ts,tsx,css,md,json}' --config ./.prettierrc",
+  "extends": [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:prettier/recommended"
+  ]
