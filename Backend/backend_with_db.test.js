5f52bfaaf8fd331460b93b7aea0d043e7324ff9d
alexhatch <alexhatchh@gmail.com>
2023-03-09 17:46:50 -0800

    Test now working (hopefully)

--- Backend/backend_with_db.test.js
+++ Backend/backend_with_db.test.js
@@ -1,88 +1,11 @@
-const myFunctions = require('./backend_with_db.js');
+const userServices = require("./models/user-services");
 
-/* test sum function */
-test('Testing sum two positive integers', () => {
-  const target = 30;
-  const result = myFunctions.sum(12, 18);
-  expect(target).toBe(result);
-});
+test("test find user by id", async () => {
+  const result = await userServices.findUserById("63ffc409f777306ea76b70dc");
 
-test('Testing sum two negative integers', () => {
-  const target = -30;
-  const result = myFunctions.sum(-12, -18);
-  expect(target).toBe(result);
+  expect(result.name).toBe("Alex Hatch");
+  expect(result.role).toBe("admin");
+  expect(result.username).toBe("alexhatch");
+  expect(result.password).toBe("05694b486bff0582fb5175cbe7cb9fb143d0d51a8335c55dd3e7d0590ee8cc5e");
+  expect(result.salt).toBe("f29ee61ebf80b36899d15a84245181c5");
 });
-
-test('Testing sum one letter one number', () => {
-    expect(() => {
-        myFunctions.sum("1", 2);
-    }).toThrow(TypeError("Please supply only numbers"));
-});
-
-test('Testing sum two letters', () => {
-    expect(() => {
-        myFunctions.sum("a", "b");
-    }).toThrow(TypeError("Please supply only numbers"));
-});
-
-/* test div function */
-test('Testing div two positive integers', () => {
-    const target = 6;
-    const result = myFunctions.div(30, 5);
-    expect(target).toBe(result);
-});
-  
-test('Testing div two negative integers', () => {
-    const target = 6;
-    const result = myFunctions.div(-30, -5);
-    expect(target).toBe(result);
-});
-  
-test('Testing div by 0', () => {
-    expect(() => {
-        myFunctions.div(5, 0);
-    }).toThrow(RangeError("Cannot divide by 0"));
-});
-
-test('Testing div two letters', () => {
-    expect(() => {
-        myFunctions.div("a", "b");
-    }).toThrow(TypeError("Please supply only numbers"));
-});
-
-test('Testing contains with number', () => {
-    const target = "abcd4f";
-    const result = myFunctions.containsNumbers(target);
-    expect(result).toBeTruthy();
-});
-
-test('Testing contains with number', () => {
-    const target = "abdefgh";
-    const result = myFunctions.containsNumbers(target);
-    expect(result).toBeFalsy();
-});
-
-test('First character is number', () => {
-    const target = "5abcdef";
-    const result = myFunctions.containsNumbers(target);
-    expect(result).toBeTruthy();
-});
-
-test('String not supplied', () => {
-    expect(() => {
-        myFunctions.containsNumbers(543);
-    }).toThrow(TypeError("Please only provide a string")
-    );
-});
-
-test('Empty string', () => {
-    const result = myFunctions.containsNumbers("");
-    expect(result).toBeFalsy();
-});
-
-test('Empty string', () => {
-    const result = myFunctions.containsNumbers("");
-    expect(result).toBeFalsy();
-});
-
-// test
